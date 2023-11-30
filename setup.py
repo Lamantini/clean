6acb775d500ecc9b5f101dafaf0f003f2c2b316a
Lamantini <lamantiniuso@gmail.com>
2023-11-30 23:44:20 +0200

    clen

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='clean_folder',
       version='1',
       description='Clean your trash files',
-      url='https://github.com/Lamantini/hometask/blob/master/sort.py',
+      url='https://github.com/Lamantini/clean',
       author='Rybchenko Anastasiia',
       author_email='lamantiniuso@gmail.com',
       packages=['clean_folder'],
